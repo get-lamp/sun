40c0df3c361b19f36dab5dddf3a62e78a013e21f
Marcelo Jacobs <mjacobs@sorcero.com>
2025-10-12 19:59:56 -0300

    Many fixes and labels

---
+++ README.md
@@ -0,0 +1,2 @@
+Sundial Ring
+
--- browser.js
+++ browser.js
@@ -4,8 +4,8 @@ const solar = require('./lib/declination');
 
 var ring = new Ring({diameter:18.5, width: 12});
 
-var oView = new View('open-ring', {factor: 10});
-//var cView = new View('closed-ring');
+var oView = new View('open-ring', {factor: 16});
+//var cView = new View('closed-ring', {factor: 16});
 
 ring.drawOpen(oView, 20, 20);
 //ring.drawClosed(cView, 100, 100);
